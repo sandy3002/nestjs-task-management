4b8130da0ca4e2a64ed56f4a213671127a86dd99
Sandipan Chatterjee <sandipan3002@gmail.com>
2025-05-27 13:23:35 +0530

    a

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from './auth/users.repository';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule, AuthModule, TypeOrmModule.forFeature([UserRepository])],
   controllers: [AppController],
   providers: [AppService],
 })
